c3e2a9803f762748886e96600b0e9649d26def97
Shannon <mathesaurus.rex@gmail.com>
2020-04-18 21:50:05 -0700

    Add garbage bin enemy prrefab

--- Assets/Scripts/RangeRestrictedEnemyController.cs
+++ Assets/Scripts/RangeRestrictedEnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OscillatingEnemyController : EnemyController
+public class RangeRestrictedEnemyController : EnemyController
 {
 
     public float minDistance;
