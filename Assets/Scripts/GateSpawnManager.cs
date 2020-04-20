7337cad9c98c115d55648297463da0d4a57b364f
DDRnJn <dhruv.ranjan@gmail.com>
2020-04-19 18:37:40 -0700

    Added logic gate pickups and spawners

---
+++ Assets/Scripts/GateSpawnManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSpawnManager : MonoBehaviour
+{
+    public Transform AndGate;
+    public Transform OrGate;
+    public Transform NotGate;
+    public Transform background;
+
+    public int maxAndGates;
+    public int maxOrGates;
+    public int maxNotGates;
+
+    public int currentAndGates;
+    public int currentOrGates;
+    public int currentNotGates;
+
+    private Vector2 getEnemySpawnPoint()
+    {
+        Vector2 backgroundSize = this.getBackgroundSize();//this.background.GetComponent<SpriteRenderer>().sprite.rect.size;
+        float xMax = backgroundSize.x / 2;
+        float yMax = backgroundSize.y / 2;
+
+        float spawnX = Random.Range(-xMax, xMax);
+        float spawnY = Random.Range(-yMax, yMax);
+
+        Vector2 randomPoint = new Vector2(spawnX, spawnY);
+        return randomPoint;
+    }
+
+    private Vector3 getBackgroundSize()
+    {
+        Vector2 backgroundSize = this.background.GetComponent<SpriteRenderer>().sprite.rect.size;
+        Vector2 scaledbackgroundSize = backgroundSize / this.background.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+
+        return scaledbackgroundSize;
+    }
+
+    void SpawnGate()
+    {
+        if (currentAndGates < maxAndGates)
+        {
+            Transform andGateSpawn = Instantiate(this.AndGate, this.getEnemySpawnPoint(), this.transform.rotation);
+            this.currentAndGates++;
+            LogicGateSpawn gateSpawn = andGateSpawn.GetComponent<LogicGateSpawn>();
+            gateSpawn.Spawner = this;
+        }
+        if (currentOrGates < maxOrGates)
+        {
+            Transform ordGateSpawn = Instantiate(this.OrGate, this.getEnemySpawnPoint(), this.transform.rotation);
+            this.currentOrGates++;
+            LogicGateSpawn gateSpawn = ordGateSpawn.GetComponent<LogicGateSpawn>();
+            gateSpawn.Spawner = this;
+        }
+        if (currentNotGates < maxNotGates)
+        {
+            Transform notGateSpawn = Instantiate(this.NotGate, this.getEnemySpawnPoint(), this.transform.rotation);
+            this.currentNotGates++;
+            LogicGateSpawn gateSpawn = notGateSpawn.GetComponent<LogicGateSpawn>();
+            gateSpawn.Spawner = this;
+        }
+    }
+
+    void Update()
+    {
+        this.SpawnGate();
+    }
+}
---
+++ Assets/Scripts/LogicGateSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicGateSpawn : MonoBehaviour
+{
+    public GateSpawnManager Spawner;
+
+    public string spawnType;
+
+    public void destroy()
+    {
+        if (Spawner)
+        {
+            if (spawnType == "AND")
+            {
+                this.Spawner.currentAndGates--;
+            }
+            if (spawnType == "OR")
+            {
+                this.Spawner.currentOrGates--;
+            }
+            if (spawnType == "NOT")
+            {
+                this.Spawner.currentNotGates--;
+            }
+        }
+        Destroy(this.gameObject);
+    }
+
+}
