311c1c81445b286513e635fa6bcf3d8482ea4f12
DDRnJn <dhruv.ranjan@gmail.com>
2020-04-20 16:50:27 -0700

    Updated instructions

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,15 +8,25 @@ public class MainMenuController : MonoBehaviour
 {
 
     public Button startButton;
+
+    public Button instructionsButton;
     // Start is called before the first frame update
     void Start()
     {
         Button button = this.startButton.GetComponent<Button>();
         button.onClick.AddListener(LoadLevel1);
+
+        Button instructionsButton = this.instructionsButton.GetComponent<Button>();
+        instructionsButton.onClick.AddListener(LoadInstructions);
     }
 
     public void LoadLevel1()
     {
         SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
     }
 }
