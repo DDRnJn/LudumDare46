f5e0db4b3d6947163842e5c5189f5ec5e4bf86b8
DDRnJn <dhruv.ranjan@gmail.com>
2020-04-20 06:28:07 -0700

    Added background music

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private static AudioController instance;
+
+    private static AudioController Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
