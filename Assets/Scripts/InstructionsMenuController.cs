3011593b62d2061ddec1bbed909b5ec4a6a06135
DDRnJn <dhruv.ranjan@gmail.com>
2020-04-20 16:54:23 -0700

    Added UI controller to instructions scene

---
+++ Assets/Scripts/InstructionsMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InstructionsMenuController : MonoBehaviour
+{
+
+    public Button menuButton;
+
+    void Start()
+    {
+        Button button = this.menuButton.GetComponent<Button>();
+        button.onClick.AddListener(LoadMainMenu);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
